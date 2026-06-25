004a51afcffa39a560eb2e9d8bb49ef5446281a3
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-06-25 23:26:41 +0530

    new files added

--- Playwright/PlaywrightPractice/tests/Testcase1.test.ts
+++ Playwright/PlaywrightPractice/tests/Testcase1.test.ts
@@ -4,7 +4,7 @@ test('Valid Register', async ({ page }) => {
   await page.goto('https://automationexercise.com/');
   await page.getByRole('link', { name: 'Signup / Login'}).click();
   await page.getByRole('textbox', { name: 'Name' }).fill('Darshan');
-  await page.locator('form').filter({ hasText: 'Signup' }).getByPlaceholder('Email Address').fill('messi123456789@gmail.com');
+  await page.locator('form').filter({ hasText: 'Signup' }).getByPlaceholder('Email Address').fill('messi12345678@gmail.com');
   await page.getByRole('button', { name: 'Signup' }).click();
   await page.locator('#password').fill('messi10');
   await page.locator('#first_name').fill('Darshan');
